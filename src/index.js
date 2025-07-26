11ecf6f6755a1568991a16dcfd02231522b63358
Lesetja <kmalapane155@gmail.com>
2025-07-26 13:12:03 +0200

    added html src file, installed packages and added a bit of css also js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import chickenImage from "./roastedchicken.png";
+import "./styles.css";
+   
+const image = document.createElement("img");
+image.src = chickenImage;
+   
+document.body.appendChild(image);
+
